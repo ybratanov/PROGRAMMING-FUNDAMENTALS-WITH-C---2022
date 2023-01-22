7e2ccf4918a69ca1fc731caab44beca0d2548995
Bratanov <111681891+ybratanov@users.noreply.github.com>
2023-01-22 08:56:42 +0100

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/07. Theatre Promotion/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Day / Age   0 <= age <= 18  18 < age <= 64  64 < age <= 122
+            //Weekday           12$	            18$	            12$
+            //Weekend           15$	            20$	            15$
+            //Holiday           5$	            12$	            10$
+
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (day == "Weekday")
+            {
+                if (0 <= age && age <= 18 || age > 64 && age <= 122)
+                {
+                    Console.WriteLine("12$");
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    Console.WriteLine("18$");
+                }
+            }
+            if (day == "Weekend")
+            {
+                if (0 <= age && age <= 18 || age > 64 && age <= 122)
+                {
+                    Console.WriteLine("15$");
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    Console.WriteLine("20$");
+                }
+            }
+            if (day == "Holiday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    Console.WriteLine("5$");
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    Console.WriteLine("12$");
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    Console.WriteLine("10$");
+                }
+            }
+            if (age < 0 || age>122)
+            {
+                Console.WriteLine("Error!");
+            }
+
+
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int currentOddNum = 1 + (i * 2);
+                Console.WriteLine(currentOddNum);
+                sum += currentOddNum;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
