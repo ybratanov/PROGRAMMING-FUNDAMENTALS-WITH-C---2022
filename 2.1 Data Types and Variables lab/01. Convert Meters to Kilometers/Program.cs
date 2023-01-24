caa3fbc9ed02b8b78e20a6b5746d6386f249ee4b
Bratanov <111681891+ybratanov@users.noreply.github.com>
2023-01-24 18:24:10 +0100

    Add files via upload

---
+++ 2.1 Data Types and Variables lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = int.Parse(Console.ReadLine());
+
+            double kilometers = meters /1000;
+            Console.WriteLine($"{kilometers:f2}") ;
+        }
+    }
+}
---
+++ 2.1 Data Types and Variables lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pounds = int.Parse(Console.ReadLine());
+
+            double dollers = pounds * 1.31;
+            Console.WriteLine($"{dollers:f3}");
+        }
+    }
+}
